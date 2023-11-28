f39df48b4f2a03d07ad7ebd1e141d809c48cf748
Sergio Lopez Martinez <82503729+ser-lo-ma@users.noreply.github.com>
2023-11-28 12:54:00 +0000

    Add files via upload

---
+++ simulation/build/CMakeFiles/sim.dir/DependInfo.cmake
@@ -0,0 +1,50 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/sergio/Documents/simulation/action.cc" "/home/sergio/Documents/simulation/build/CMakeFiles/sim.dir/action.cc.o"
+  "/home/sergio/Documents/simulation/construction.cc" "/home/sergio/Documents/simulation/build/CMakeFiles/sim.dir/construction.cc.o"
+  "/home/sergio/Documents/simulation/generator.cc" "/home/sergio/Documents/simulation/build/CMakeFiles/sim.dir/generator.cc.o"
+  "/home/sergio/Documents/simulation/physics.cc" "/home/sergio/Documents/simulation/build/CMakeFiles/sim.dir/physics.cc.o"
+  "/home/sergio/Documents/simulation/sim.cc" "/home/sergio/Documents/simulation/build/CMakeFiles/sim.dir/sim.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "G4LIB_BUILD_DLL"
+  "G4UI_USE_QT"
+  "G4UI_USE_TCSH"
+  "G4VIS_USE_OPENGL"
+  "G4VIS_USE_OPENGLQT"
+  "G4VIS_USE_RAYTRACERX"
+  "G4VIS_USE_TOOLSSG_QT_GLES"
+  "PTL_BUILD_DLL"
+  "QT_CORE_LIB"
+  "QT_GUI_LIB"
+  "QT_NO_DEBUG"
+  "QT_OPENGL_LIB"
+  "QT_PRINTSUPPORT_LIB"
+  "QT_WIDGETS_LIB"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/sergio/software/geant4/geant4-v11.1.3-install/include/Geant4"
+  "/usr/include/x86_64-linux-gnu/qt5"
+  "/usr/include/x86_64-linux-gnu/qt5/QtCore"
+  "/usr/lib/x86_64-linux-gnu/qt5/mkspecs/linux-g++"
+  "/usr/include/x86_64-linux-gnu/qt5/QtGui"
+  "/usr/include/x86_64-linux-gnu/qt5/QtWidgets"
+  "/usr/include/x86_64-linux-gnu/qt5/QtOpenGL"
+  "/usr/include/x86_64-linux-gnu/qt5/QtPrintSupport"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ simulation/generator.cc
@@ -0,0 +1,34 @@
+#include"generator.hh"
+
+MyPrimaryGenerator::MyPrimaryGenerator(){
+//Creates only one particle per event run
+    fParticleGun = new G4ParticleGun(1);
+}
+
+
+MyPrimaryGenerator::~MyPrimaryGenerator(){
+    delete fParticleGun;
+}
+
+void MyPrimaryGenerator::GeneratePrimaries(G4Event *anEvent){
+// Create the definition of the particle from the particle table
+    G4ParticleTable *particletable = G4ParticleTable::GetParticletable();
+    G4string particleName = "proton";
+    G4ParticleDefinition *particle = particleTable -> FindParticle(particleName);
+
+
+// Create the vector for the position of the particle and the direction of its momentum
+    G4ThreeVector pos(0.,0.,0.);
+    G4ThreeVector mom(0.,0.,1.);
+
+// Use the particle gun and set the parameters of the particles emitted
+    fParticleGun->SetParticlePosition(pos);
+    fParticleGun->SetParticleMomentumDirection(mom);
+    fParticleGun->SetParticleMomentum(100.*GeV);
+    fParticleGun->SetParticleDefinition(particle);
+
+// Generate the primary vertex of the particle
+
+    fParticleGun->GeneratePrimaryVertex(anEvent);
+
+}
